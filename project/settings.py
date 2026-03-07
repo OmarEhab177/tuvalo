331adf5145bb51ab19311cdf63e7d07c027f6348
Omar <omarehap17@gmail.com>
2026-03-07 20:07:36 +0200

    add requirements file

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+x+vdj$@doem=w#-=8+sun0t0h0k%_j5bvek9a=y&349gf+=&+"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-+x+vdj$@doem=w#-=8+sun0t0h0k%_j5bvek9a=y&349gf+=&+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
 
 
 # Application definition
